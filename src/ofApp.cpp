3560bbb5e390e6a1c36e361ab4ba48a9582b8603
functorial <alexpokorny92@gmail.com>
2021-09-10 14:15:17 -0700

    small changes to int types and moving around functions

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,24 +1,24 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void sense_slimes(slimeSystem& system) {
+void ofApp::sense_slimes(slimeSystem &system) {
 
-	for (unsigned int i = 0; i < system.vec.size(); i++) {
+	for (uint32_t i = 0; i < system.vec.size(); i++) {
 		slime* s = &system.vec[i];
 
 		// Sense pheremones
 		float lsense = 0;
 		float csense = 0;
 		float rsense = 0;
-		for (unsigned int k = 0; k < system.attractSystems.size(); k++) {
-			for (unsigned int j = 0; j < (system.attractSystems[k])->vec.size(); j++) {
+		for (uint8_t k = 0; k < system.attractSystems.size(); k++) {
+			for (uint32_t j = 0; j < (system.attractSystems[k])->vec.size(); j++) {
 				if (((system.attractSystems[k])->vec[j].pos - s->lsensor.pos).length() <= s->lsensor.rad) { lsense += 1 * system.attractStrength; }
 				if (((system.attractSystems[k])->vec[j].pos - s->csensor.pos).length() <= s->csensor.rad) { csense += 1 * system.attractStrength; }
 				if (((system.attractSystems[k])->vec[j].pos - s->rsensor.pos).length() <= s->rsensor.rad) { rsense += 1 * system.attractStrength; }
 			}
 		}
-		for (unsigned int k = 0; k < system.repelSystems.size(); k++) {
-			for (unsigned int j = 0; j < (system.repelSystems[k])->vec.size(); j++) {
+		for (uint8_t k = 0; k < system.repelSystems.size(); k++) {
+			for (uint32_t j = 0; j < (system.repelSystems[k])->vec.size(); j++) {
 				if (((system.repelSystems[k])->vec[j].pos - s->lsensor.pos).length() <= s->lsensor.rad) { lsense -= 1 * system.repelStrength; }
 				if (((system.repelSystems[k])->vec[j].pos - s->csensor.pos).length() <= s->csensor.rad) { csense -= 1 * system.repelStrength; }
 				if (((system.repelSystems[k])->vec[j].pos - s->rsensor.pos).length() <= s->rsensor.rad) { rsense -= 1 * system.repelStrength; }
@@ -27,7 +27,7 @@ void sense_slimes(slimeSystem& system) {
 
 		// Rotate slime based on pheremone readings
 		// Case1: Turn left
-		int n;
+		int8_t n;
 		if (lsense > csense && csense >= rsense) {
 			n = 1;
 		}
@@ -53,8 +53,8 @@ void sense_slimes(slimeSystem& system) {
 }
 
 //--------------------------------------------------------------
-void move_slimes(slimeSystem& system) {
-	for (unsigned int i = 0; i < system.vec.size(); i++) {
+void ofApp::move_slimes(slimeSystem& system) {
+	for (uint32_t i = 0; i < system.vec.size(); i++) {
 		slime* s = &system.vec[i];
 
 		// Move particle
@@ -89,8 +89,8 @@ void move_slimes(slimeSystem& system) {
 }
 
 //--------------------------------------------------------------
-void draw_slimes(slimeSystem& system, int size) {
-	for (unsigned int i = 0; i < system.vec.size(); i++) {
+void ofApp::draw_slimes(slimeSystem& system, uint8_t size) {
+	for (uint32_t i = 0; i < system.vec.size(); i++) {
 		ofSetColor(system.sli_col);
 		ofDrawCircle(system.vec[i].pos, size);
 		ofSetColor(system.sen_col);
@@ -110,7 +110,7 @@ void ofApp::setup() {
 	record.setWidth(ofGetWidth());
 	record.setHeight(ofGetHeight());
 	record.setFFmpegPath(ofToDataPath("ffmpeg.exe"));
-	record.setFps(60);
+	record.setFps(60.0f);
 	record.setOutputPath(ofToDataPath("slime_" + ofToString(ofGetSystemTimeMillis()) + ".mp4", true));
 	record.startCustomRecord();
 	isRecordingVideo = true;
@@ -118,78 +118,52 @@ void ofApp::setup() {
 
 	// Set up a slime system RED
 	//--------------------------------------------------------------------------------------------------------------------------------------------------
-	int num_slimes1 = 100;
-	slimes1.s_ang = 50;		// degrees
-	slimes1.s_dis = 20;		// pixels
-	slimes1.s_rad = 15;		// pixels
-	slimes1.sen_col = ofColor(255, 50, 50, 5);	// RGB + transparency
-	slimes1.sli_col = ofColor(255, 100, 100);
+	slimes1.s_ang = 50.0f;		// degrees
+	slimes1.s_dis = 20.0f;		// pixels
+	slimes1.s_rad = 15.0f;		// pixels
+	slimes1.sen_col = ofColor(255.0f, 50.0f, 50.0f, 5.0f);	// RGB + transparency
+	slimes1.sli_col = ofColor(255.0f, 100.0f, 100.0f);
 
 
 	slimes1.attractSystems = vector<slimeSystem*>{ &slimes1 };
-	slimes1.attractStrength = 1.0;
+	slimes1.attractStrength = 1.0f;
 	slimes1.repelSystems = vector<slimeSystem*>{ &slimes2, &slimes3 };
-	slimes1.repelStrength = -0.5;
+	slimes1.repelStrength = -0.5f;
 
-	for (int i = 0; i < num_slimes1; ++i) {
+	const uint32_t num_slimes1 = 100;
+	slime slimes1_arr[num_slimes1];
+	for (uint32_t i = 0; i < num_slimes1; ++i) {
 
 		// Random initial pos and vel
-		ofVec2f pos = ofVec2f(ofRandom(0, ofGetWidth()),
-							  ofRandom(0, ofGetHeight()));
-		ofVec2f vel = ofVec2f(ofRandom(-5, 5),
-							  ofRandom(-5, 5));
-
-		/*
-		// Grid-like initial pos and constant initial vel
-		ofVec2f pos = ofVec2f(50 * i, 50 * i);
-		ofVec2f vel = ofVec2f(2, -2);
-		*/
-
-		/*
-		// Elliptic initial pos and vel
-		float init_rad1 = 50;
-		ofVec2f pos = ofVec2f(init_rad1 * cosf(44.0 / 7.0 * float(i) / float(num_slimes1)) + ofGetWidth() / 2, init_rad1 / 2.0 * sinf(44.0 / 7.0 * float(i) / float(num_slimes1)) + ofGetHeight() / 2);
-		ofVec2f vel = 3 * ofVec2f(cosf(44.0 / 7.0 * float(i) / float(num_slimes1)), sinf(44.0 / 7.0 * float(i) / float(num_slimes1)));
-		*/
+		ofVec2f pos = ofVec2f(ofRandom(0.0f, ofGetWidth()),
+							  ofRandom(0.0f, ofGetHeight()));
+		ofVec2f vel = ofVec2f(ofRandom(-5.0f, 5.0f),
+							  ofRandom(-5.0f, 5.0f));
 
 		slime s = slime(pos, vel, slimes1.s_ang, slimes1.s_dis, slimes1.s_rad);
-		slimes1.vec.push_back(s);
-
+		slimes1_arr[i] = s;
 	}
 
 	// Set up a slime system BLUE
 	//--------------------------------------------------------------------------------------------------------------------------------------------------
-	int num_slimes2 = 200;
-	slimes2.s_ang = 40;		// degrees
-	slimes2.s_dis = 30;		// pixels
-	slimes2.s_rad = 15;		// pixels
-	slimes2.sen_col = ofColor(50, 50, 255, 5);	// RGB + transparency
-	slimes2.sli_col = ofColor(100, 100, 255);
+	uint32_t num_slimes2 = 200;
+	slimes2.s_ang = 40.0f;		// degrees
+	slimes2.s_dis = 30.0f;		// pixels
+	slimes2.s_rad = 15.0f;		// pixels
+	slimes2.sen_col = ofColor(50.0f, 50.0f, 255.0f, 5.0f);	// RGB + transparency
+	slimes2.sli_col = ofColor(100.0f, 100.0f, 255.0f);
 	slimes2.attractSystems = vector<slimeSystem*>{ &slimes2 };
-	slimes2.attractStrength = 1.0;
+	slimes2.attractStrength = 1.0f;
 	slimes2.repelSystems = vector<slimeSystem*>{ &slimes1, &slimes3 };
-	slimes2.repelStrength = 3.0;
+	slimes2.repelStrength = 3.0f;
 
-	for (int i = 0; i < num_slimes2; ++i) {
+	for (uint32_t i = 0; i < num_slimes2; ++i) {
 
 		// Random initial pos and vel
-		ofVec2f pos = ofVec2f(ofRandom(0, ofGetWidth()),
-							  ofRandom(0, ofGetHeight()));
-		ofVec2f vel = ofVec2f(ofRandom(-3.5, 3.5),
-							  ofRandom(-3.5, 3.5));
-
-		/*
-		// Grid-like initial pos and constant initial vel
-		ofVec2f pos = ofVec2f(50 * i, 50 * i);
-		ofVec2f vel = ofVec2f(2, -2);
-		*/
-
-		/*
-		// Elliptic initial pos and vel
-		float init_rad2 = 50;
-		ofVec2f pos = 0.5 * ofVec2f(init_rad2 * cosf(44.0 / 7.0 * float(i) / float(num_slimes2)) + ofGetWidth() / 2, init_rad2 / 2.0 * sinf(44.0 / 7.0 * float(i) / float(num_slimes2)) + ofGetHeight() / 2);
-		ofVec2f vel = 2 * ofVec2f(cosf(44.0 / 7.0 * float(i) / float(num_slimes2)), 0.5 * sinf(44.0 / 7.0 * float(i) / float(num_slimes2)));
-		*/
+		ofVec2f pos = ofVec2f(ofRandom(0.0f, ofGetWidth()),
+							  ofRandom(0.0f, ofGetHeight()));
+		ofVec2f vel = ofVec2f(ofRandom(-3.5f, 3.5f),
+							  ofRandom(-3.5f, 3.5f));
 
 		slime s = slime(pos, vel, slimes2.s_ang, slimes2.s_dis, slimes2.s_rad);
 		slimes2.vec.push_back(s);
@@ -197,37 +171,24 @@ void ofApp::setup() {
 
 	// Set up a slime system GREEN
 	//--------------------------------------------------------------------------------------------------------------------------------------------------
-	int num_slimes3 = 200;
-	slimes3.s_ang = 40;	// degrees
-	slimes3.s_dis = 20;	// pixels
-	slimes3.s_rad = 15;	// pixels
-	slimes3.sen_col = ofColor(50, 255, 50, 2);	// RGB + transparency
-	slimes3.sli_col = ofColor(100, 255, 100);
+	uint32_t num_slimes3 = 200;
+	slimes3.s_ang = 40.0f;	// degrees
+	slimes3.s_dis = 20.0f;	// pixels
+	slimes3.s_rad = 15.0f;	// pixels
+	slimes3.sen_col = ofColor(50.0f, 255.0f, 50.0f, 2.0f);	// RGB + transparency
+	slimes3.sli_col = ofColor(100.0f, 255.0f, 100.0f);
 	slimes3.attractSystems = vector<slimeSystem*>{ &slimes3 };
-	slimes3.attractStrength = 1.0;
+	slimes3.attractStrength = 1.0f;
 	slimes3.repelSystems = vector<slimeSystem*>{ &slimes2, &slimes1 };
-	slimes3.repelStrength = 0.3;
+	slimes3.repelStrength = 0.3f;
 
-	for (int i = 0; i < num_slimes2; ++i) {
+	for (uint32_t i = 0; i < num_slimes2; ++i) {
 
 		// Random initial pos and vel
 		ofVec2f pos = ofVec2f(ofRandom(0, ofGetWidth()),
 							  ofRandom(0, ofGetHeight()));
 		ofVec2f vel = ofVec2f(ofRandom(-2, 2),
 							  ofRandom(-2, 2));
-
-		/*
-		// Grid-like initial pos and constant initial vel
-		ofVec2f pos = ofVec2f(50 * i, 50 * i);
-		ofVec2f vel = ofVec2f(2, -2);
-		*/
-
-		/*
-		// Elliptic initial pos and vel
-		float init_rad3 = 50;
-		ofVec2f pos = 1.5 * ofVec2f(init_rad3 * cosf(44.0 / 7.0 * float(i) / float(num_slimes3)) + ofGetWidth() / 2, init_rad3 / 2.0 * sinf(44.0 / 7.0 * float(i) / float(num_slimes3)) + ofGetHeight() / 2);
-		ofVec2f vel = ofVec2f(cosf(44.0 / 7.0 * float(i) / float(num_slimes3)), 0.5 * sinf(44.0 / 7.0 * float(i) / float(num_slimes3)));
-		*/
 
 		slime s = slime(pos, vel, slimes3.s_ang, slimes3.s_dis, slimes3.s_rad);
 		slimes3.vec.push_back(s);
@@ -237,14 +198,14 @@ void ofApp::setup() {
 //--------------------------------------------------------------
 void ofApp::update() {
 
-	sense_slimes(slimes1);
-	move_slimes(slimes1);
+	ofApp::sense_slimes(slimes1);
+	ofApp::move_slimes(slimes1);
 
-	sense_slimes(slimes2);
-	move_slimes(slimes2);
+	ofApp::sense_slimes(slimes2);
+	ofApp::move_slimes(slimes2);
 
-	sense_slimes(slimes3);
-	move_slimes(slimes3);
+	ofApp::sense_slimes(slimes3);
+	ofApp::move_slimes(slimes3);
 }
 
 //--------------------------------------------------------------
@@ -259,11 +220,11 @@ void ofApp::draw() {
 	}
 
 	fbo.begin();
-	ofClear(255, 255, 255, 0);
-	ofBackground(0, 0, 0);
-	draw_slimes(slimes1, 2);
-	draw_slimes(slimes2, 2);
-	draw_slimes(slimes3, 2);
+	ofClear(255.0f, 255.0f, 255.0f, 0.0f);
+	ofBackground(0);
+	ofApp::draw_slimes(slimes1, 2);
+	ofApp::draw_slimes(slimes2, 2);
+	ofApp::draw_slimes(slimes3, 2);
 	fbo.end();
 
 	fbo.draw(0, 0);
--- src/ofApp.h
+++ src/ofApp.h
@@ -3,6 +3,7 @@
 #include "ofMain.h"
 #include "ofxFFmpegRecorder.h"
 #include "../slimeSystem.h"
+#include <stdint.h>
 
 class ofApp : public ofBaseApp {
 
@@ -13,10 +14,16 @@ public:
 
 	void keyPressed(int key);
 
+	void sense_slimes(slimeSystem &slimes);
+	void move_slimes(slimeSystem &slimes);
+	void draw_slimes(slimeSystem &slimes, uint8_t size);
+
 	ofxFFmpegRecorder record;
 	ofFbo fbo;
 	ofPixels pixel;
 	bool isRecordingVideo;
 
 	slimeSystem slimes1, slimes2, slimes3;
+
+
 };
